2cdb4653e4429c0ad995539a83d29499e927dc40
Bubu <a0910499399@gmail.com>
2022-11-13 02:03:18 +0800

    Java 頁面跳轉到 Kotlin 頁面，環境(兩個gradle)已配置好

---
+++ app/src/main/java/com/example/java1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.java1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // test_btnInsert切換畫面到kotlin insert food Activity
+        Button test_btnInsert=(Button) findViewById(R.id.test_btnInsert);
+        test_btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 切換 Activity
+                Intent intent_insert=new Intent(MainActivity.this,InsertActivity.class);
+                startActivity(intent_insert);
+
+                System.out.println("按鈕已按下");
+            }
+        });
+    }
+}
